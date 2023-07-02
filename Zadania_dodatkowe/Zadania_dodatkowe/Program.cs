77a4b34d15d6f7290aa63b03c162fb848fe584b6
Marciano111 <mgemz@DESKTOP-MKTAA21>
2023-07-02 13:44:03 +0200

    Prosty kalkulator

--- Zadania_dodatkowe/Zadania_dodatkowe/Program.cs
+++ Zadania_dodatkowe/Zadania_dodatkowe/Program.cs
@@ -1,27 +1,109 @@
-﻿// Gra w zgadywanie liczby
+﻿Console.WriteLine("**********************");
+Console.WriteLine("***** Calculator *****");
+Console.WriteLine("**********************");
+Console.WriteLine("*****    MENU    *****");
+Console.WriteLine("**********************");
+Console.WriteLine("* 1 - Addition       *");
+Console.WriteLine("* 2 - Subtraction    *");
+Console.WriteLine("* 3 - Multiplication *");
+Console.WriteLine("* 4 - Division       *");
+Console.WriteLine("**********************");
+Console.WriteLine();
 
-Random random = new Random();
-var random_number = random.Next(0, 101);
-var attemts_number = 1;
+var number1Correct = false;
+var number2Correct = false;
+var operationCorrect = false;
 
-while (true)
+var number1 = 0f;
+var number2 = 0f;
+var operationNumber = 0;
+
+while (!(number1Correct && number2Correct && operationCorrect))  
 {
-    Console.Write("Guess the drawn number: ");
-    var guessed_number = int.Parse(Console.ReadLine());
+    if (!number1Correct)  
+    {
+        Console.WriteLine("Enter the first number: ");
+        if (float.TryParse(Console.ReadLine(), out float result1))
+        {
+            number1 = result1;
+            number1Correct = true;
+        }
+        else
+        {
+            Console.WriteLine("You must enter the number!");
+            continue;
+        }
+    }
+    if (!number2Correct)
+    {
+        Console.WriteLine("Enter the second number: ");
+        if (float.TryParse(Console.ReadLine(), out float result2))
+        {
+            number2 = result2;
+            number2Correct = true;
+        }
+        else
+        {
+            Console.WriteLine("You must enter the number!");
+            continue;
+        }
+    }
+    if (!operationCorrect)
+    {
+        Console.WriteLine("Enter the operation number from the MENU: ");
+        if(int.TryParse(Console.ReadLine(), out int result3))
+        {
+            operationNumber = result3;
+            operationCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("You must enter the number!");
+            continue;
+        }
 
-    if (guessed_number == random_number)
-    {
-        Console.WriteLine("Congratulations! You guessed it {0} times!", attemts_number);
-        break;
+        switch (operationNumber) 
+        {
+            case 1:
+                {
+                    var additionScore = number1 + number2;
+                    Console.WriteLine($"The result of adding is {additionScore}");
+                    break;
+                }
+            case 2:
+                {
+                    var subtractionScore = number1 - number2;
+                    Console.WriteLine($"The result of subtraction is {subtractionScore}");
+                    break;
+                }
+            case 3:
+                {
+                    var multiplicationScore = number1 * number2;
+                    Console.WriteLine($"The result of multiplication is {multiplicationScore}");
+                    break;
+                }
+            case 4:
+                {
+                    if (number2 != 0)
+                    {
+                        var divisionScore = number1 / number2;
+                        Console.WriteLine($"The result of division is {divisionScore:N2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Division by zero is not allowed!");
+                    }
+                    break;
+                }
+            default: 
+                {
+                    Console.WriteLine("No such option in the Menu!");
+                    break;
+                }
+
+
+        }
     }
-    else if (guessed_number > random_number)
-    {
-        Console.WriteLine("Enter a lower number...");
-        attemts_number++;
-    }
-    else if (guessed_number < random_number)
-    {
-        Console.WriteLine("Enter a higher number...");
-        attemts_number++;
-    }
-};
+}
+
+
